9d8f4dbb580e2e92f46d2552f530d11cd9700078
pylaply <munu2635@naver.com>
2018-05-04 02:48:43 +0000

    Level_order

--- algorithm/Tree/level_order_tree.c
+++ algorithm/Tree/level_order_tree.c
@@ -1,57 +1,35 @@
 #include "node.h"
 
-extern void InitializeStack();
-extern void Push(Node *);
-extern Node* Pop();
-extern int IsStackEmpty();
+extern void InitializeQueue();
+extern void Put(Node *);
+extern Node* Get();
+extern int IsQueueEmpty();
 
 extern void InitializeTree();
 extern void MakeTree();
 
 void Visit(Node *);
 void Recursive_Traverse(Node *);
-void Stack_Traverse(Node *);
+void Queue_Traverse(Node *);
 
 Node *Parent, *LeftChild, *RightChild;
 Node *HeadNode, *EndNode;
 
 void Recursive_Traverse(Node *ptrNode){
 	if(ptrNode != EndNode){
+		Visit(ptrNode);
 		Recursive_Traverse(ptrNode->Left);
 		Recursive_Traverse(ptrNode->Right);
-		Visit(ptrNode);
 	}
 }
 
-void Stack_Traverse(Node *ptrNode){
+void Queue_Traverse(Node *ptrNode){
 	int Finish = 0;
-	Node *ptrVisited = EndNode;
-	Node *ptrPushed = EndNode;
-
 	do{
-		while(ptrNode != EndNode && ptrNode != ptrVisited){
-			if(ptrNode != ptrPushed)
-				Push(ptrNode);
-			if(ptrNode->Right != EndNode)
-				Push(ptrNode -> Right);
-			if(ptrNode->Left != EndNode)
-				Push(ptrNode->Left);
-
-			ptrPushed = ptrNode->Left;
-			ptrNode = ptrNode->Left;
+		while(ptrNode != EndNode){
 		}
 		if(!IsStackEmpty()){
-			ptrNode = Pop();
 
-			if(ptrNode->Left != EndNode && ptrNode->Right == EndNode && ptrNode->Left != ptrVisited){
-				Push(ptrNode);
-				ptrNode = ptrNode->Left;
-			}
-
-			if(ptrNode->Right == EndNode || ptrNode->Right == ptrVisited){
-				Visit(ptrNode);
-				ptrVisited = ptrNode;
-			}
 		}
 		else
 			Finish = 1;
