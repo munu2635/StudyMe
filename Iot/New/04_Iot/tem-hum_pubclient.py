0a6495e884b0f666a0a1ae43855fd688747a41a0
Laply <munu2635@naver.com>
2018-05-12 16:27:30 +0900

    iot-4

--- Iot/New/04_Iot/tem-hum_pubclient.py
+++ Iot/New/04_Iot/tem-hum_pubclient.py
@@ -1,8 +1,13 @@
 import paho.mqtt.client as mqtt
+import RPi.GPIO as gpio
+import dht11
 import time
+import dateTime
 
-
-# use onse data
+gpio.setwarnings(False)
+gpio.setmode(gpio.BCM)
+gpio.cleanup()
+instance =dht11.DHT11(pin = 5)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -19,8 +24,11 @@ mqttc.loop_start()
 try:
     num = 0
     while True:
-	tem = get1()
-	hum = get2()
+
+	result = instance.read()
+	if result.is_valid():
+	    tem = result.temperature
+	    hum = result.humidity
         if num == 0 :
             mqttc.publish("environment/temperature", tem)
             time.sleep(2)
@@ -34,5 +42,6 @@ try:
 
 except KeyboardInterrupt:
     print("Finished!")
+    gpio.cleanup()
     mqttc.loop_stop()
     mqttc.disconnect()
